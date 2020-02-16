ec9a76cd9915b6b4c738582c35214fd549deba26
kiryl85 <akiryluk@gmail.com>
2020-02-16 22:22:53 +0100

    Update fastled_config.h

--- fastled_config.h
+++ fastled_config.h
@@ -61,6 +61,9 @@
 #define FASTLED_INTERRUPT_RETRY_COUNT 2
 #endif
 
+// Use this to add RGBW support to the clockless ESP8266 driver
+// #define FASTLED_RGBW 
+
 // Use this toggle to enable global brightness in contollers that support is (ADA102 and SK9822).
 // It changes how color scaling works and uses global brightness before scaling down color values.
 // This enable much more accurate color control on low brightness settings.
