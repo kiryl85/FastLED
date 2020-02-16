717f0d802e00ebfb1de9653c85d5e247acfdf8f4
kiryl85 <akiryluk@gmail.com>
2020-02-16 22:33:20 +0100

    Update clockless_esp8266.h

--- platforms/esp/8266/clockless_esp8266.h
+++ platforms/esp/8266/clockless_esp8266.h
@@ -7,12 +7,24 @@ extern uint32_t _frame_cnt;
 extern uint32_t _retry_cnt;
 #endif
 
+#ifdef FASTLED_HAS_PRAGMA_MESSAGE
+#    pragma message "Using RGBW controller as per FASTLED_RGBW define"
+#else
+#    warning "Using RGBW controller as per FASTLED_RGBW define"
+#endif
+
 // Info on reading cycle counter from https://github.com/kbeckmann/nodemcu-firmware/blob/ws2812-dual/app/modules/ws2812.c
 __attribute__ ((always_inline)) inline static uint32_t __clock_cycles() {
   uint32_t cyc;
   __asm__ __volatile__ ("rsr %0,ccount":"=a" (cyc));
   return cyc;
 }
+
+typedef struct {
+    uint32_t c0;
+    uint32_t c1;
+    uint32_t c2;
+} out_4px; 
 
 #define FASTLED_HAS_CLOCKLESS 1
 
@@ -73,12 +85,41 @@ protected:
 	static uint32_t ICACHE_RAM_ATTR showRGBInternal(PixelController<RGB_ORDER> pixels) {
 		// Setup the pixel controller and load/scale the first byte
 		pixels.preStepFirstByteDithering();
+	#ifdef FASTLED_RGBW
+		register out_4px output;
+		register uint32_t minc;
+		output.c0 = pixels.loadAndScale0();
+	#else
 		register uint32_t b = pixels.loadAndScale0();
+	#endif // FASTLED_RGBW
     pixels.preStepFirstByteDithering();
 		os_intr_lock();
     uint32_t start = __clock_cycles();
 		uint32_t last_mark = start;
 		while(pixels.has(1)) {
+ #ifdef FASTLED_RGBW
+            output.c1 = pixels.loadAndScale1();
+            output.c2 = pixels.loadAndScale2();
+
+            minc = min(output.c0, output.c1);
+            minc = min(output.c2, minc);
+
+            writeBits<8+XTRA0>(last_mark, output.c0 - minc);
+
+            // Write first byte, read next byte
+
+			// Write second byte, read 3rd byte
+			writeBits<8+XTRA0>(last_mark, output.c1 - minc);
+
+			// Write third byte, read 1st byte of next pixel
+			writeBits<8+XTRA0>(last_mark, output.c2 - minc);
+
+			// write out all 0's for the 4th byte for RGBW strips - this is a
+			// hack and will break your RGB WS2812 strips.
+			writeBits<8+XTRA0>(last_mark, minc);
+
+            output.c0 = pixels.advanceAndLoadAndScale0();
+#else		
 			// Write first byte, read next byte
 			writeBits<8+XTRA0>(last_mark, b);
 			b = pixels.loadAndScale1();
@@ -90,7 +131,7 @@ protected:
 			// Write third byte, read 1st byte of next pixel
 			writeBits<8+XTRA0>(last_mark, b);
       b = pixels.advanceAndLoadAndScale0();
-
+#endif // FASTLED_RGBW 
 			#if (FASTLED_ALLOW_INTERRUPTS == 1)
 			os_intr_unlock();
 			#endif
